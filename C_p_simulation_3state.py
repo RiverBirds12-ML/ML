Fixed the sign of exp

C_p_simulation_3state.py
@@ -5,13 +5,13 @@ from scipy.integrate import quad
 alpha = 0.01
 # gamma = Ae^{-t/tau} + B
 tau = 50
-A = 0.00
+A = 0.01
 B = 0.02
 
 N = 10000
 
 tmax = 100
-dt = 0.01
+dt = 0.1
 time_steps = np.arange(0, tmax+dt, dt)
 
 state = np.zeros(N)
@@ -24,10 +24,20 @@ N_C_rec = []
 N_I_rec = []
 t_rec = []
 Sumation = []
-Oo = 0
+Oo = 0.1*N
+state[:int(Oo)] = 1
 N_O_sim = Oo
 N_C_sim = N - Oo
 N_I_sim = 0
+
+N_O_sim2 = Oo
+N_C_sim2 = N - Oo
+N_I_sim2 = 0
+N_O_rec2 = []
+N_C_rec2 = []
+N_I_rec2 = []
+
+O_term_rec = []
 
 
 
@@ -49,11 +59,12 @@ for t in time_steps:
     #dNO = dt*(alpha*N_C_sim - dt*sum(alpha*N_C_rec)*(A*np.exp(-(t - t_rec)/tau)+B)*(-A*(np.exp(-t/tau)-np.exp(-t_rec/tau)) + B*(t-t_rec) ) - (A*np.exp(-t/tau)+B)*Oo*np.exp( -sum( A* (np.exp(-(t - t_rec)/tau)+B )   ) ) )
     array1 = alpha*np.array(N_C_rec)
     array2 = A * np.exp( -(t - np.array(t_rec)) / tau ) + B
-    array3 = np.exp(A*tau*(np.exp(-(t-np.array(t_rec))/tau)-1) + B*(t-np.array(t_rec)) )
+    #array3 = np.exp(A*tau*(np.exp(-(t-np.array(t_rec))/tau)-1) + B*(t-np.array(t_rec)) )
+    array3 = np.exp(A*tau*(np.exp(-(t-np.array(t_rec))/tau)-1) - B*(t-np.array(t_rec)) )
     #dNO = dt*(alpha*N_C_sim - dt*sum (  (alpha*np.array(N_C_rec))*(A*np.exp(-(t - np.array(t_rec))/tau)+B)*(A*(np.exp(-t/tau)-np.exp(-np.array(t_rec)/tau)) - B*(t-np.array(t_rec)) ) ) - (A*np.exp(-t/tau)+B)*Oo*np.exp( -A*tau*np.exp(-t/tau) + B*t + A*tau ) )
-    dNO = dt*(alpha*N_C_sim - dt*dt*np.sum(array1*array2*array3) )
-    #dNO = dt*(alpha*N_C_sim - dt*np.sum(array1*array2*array3) - (A*np.exp(-t/tau)+B)*Oo*np.exp( -A*tau*np.exp(-t/tau) + B*t + A*tau ) )
-
+    #dNO = dt*(alpha*N_C_sim - dt*np.sum(array1*array2*array3) )
+    dNO = dt*(alpha*N_C_sim - dt*np.sum(array1*array2*array3) - (A*np.exp(-t/tau)+B)*Oo*np.exp( A*tau*np.exp(-t/tau) - B*t - A*tau ) )
+    O_term_rec.append(dt*np.sum(array1*array2*array3)/B)
     dNI = -dNC-dNO
 
     N_C_sim += dNC
@@ -65,6 +76,17 @@ for t in time_steps:
     N_C_rec.append(N_C_sim)
     t_rec.append(t)
 
+    # Mean-Field
+    dNC2 = dt*(-alpha*N_C_sim2)
+    dNO2 = dt*(alpha*N_C_sim2 - B*N_O_sim2)
+    dNI2 = -dNC2-dNO2
+    N_C_sim2 += dNC2
+    N_O_sim2 += dNO2
+    N_I_sim2 += dNI2
+    N_O_rec2.append(N_O_sim2)
+    N_I_rec2.append(N_I_sim2)
+    N_C_rec2.append(N_C_sim2)
+
     Sumation.append(np.sum(array1*array2*array3))
 
     if t % 10 == 0:
@@ -73,17 +95,15 @@ for t in time_steps:
 plt.figure(figsize=(9,9))
 plt.plot(t_rec,N_C)
 plt.plot(t_rec,N_O_rec)
+plt.plot(t_rec,N_O_rec2)
+#plt.plot(t_rec,O_term_rec)
 plt.xlabel('t')
 plt.ylabel('N_O')
-plt.legend(['Markov', 'Analytical'])
-
-plt.figure(figsize=(9,9))
-plt.plot(array1)
-plt.figure(figsize=(9,9))
-plt.plot(array2)
-plt.figure(figsize=(9,9))
-plt.plot(array3)
+plt.legend(['Markov', 'Analytical', 'Mean Field', 'O_term_rec'])
+#plt.legend(['Markov', 'Analytical', 'Mean Field', 'O_term_rec'])
 plt.show()
+
+
 
 # N_C_inst = []
 # N_C_pred_inst = []